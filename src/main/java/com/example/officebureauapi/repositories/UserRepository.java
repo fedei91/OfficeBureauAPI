83374be01ed764c78606c30646fa36e02c2c1f14
Federico <fedej.izaguirre@gmail.com>
2023-12-22 20:20:23 +0100

    change return type

--- src/main/java/com/example/officebureauapi/repositories/UserRepository.java
+++ src/main/java/com/example/officebureauapi/repositories/UserRepository.java
@@ -4,9 +4,10 @@ import com.example.officebureauapi.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
