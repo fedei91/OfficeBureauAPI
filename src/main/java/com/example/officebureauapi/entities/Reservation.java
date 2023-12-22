cee045385b6d0df9271724edb8bf15b1e5817ed9
Federico <fedej.izaguirre@gmail.com>
2023-12-22 20:21:13 +0100

    include auditing fields

--- src/main/java/com/example/officebureauapi/entities/Reservation.java
+++ src/main/java/com/example/officebureauapi/entities/Reservation.java
@@ -3,8 +3,11 @@ package com.example.officebureauapi.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -15,6 +18,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "reservations")
 public class Reservation {
     @Id
@@ -29,9 +33,18 @@ public class Reservation {
     private String desktopId;
 
     @CreatedDate
-    private LocalDateTime startTstamp;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdTstamp;
     @LastModifiedDate
-    private LocalDateTime lastTstamp;
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedTstamp;
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
 
     @Column(name = "is_deleted", columnDefinition = "boolean default false", nullable = false)
     private boolean isDeleted = false;
