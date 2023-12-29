894d4c8e459ff6e3b3086ca2d45e7288ce3611a6
Federico <fedej.izaguirre@gmail.com>
2023-12-29 11:16:49 +0100

    fix null fields

--- src/main/java/com/example/officebureauapi/dto/UserDto.java
+++ src/main/java/com/example/officebureauapi/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.example.officebureauapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.extern.jackson.Jacksonized;
 
@@ -10,6 +11,7 @@ import lombok.extern.jackson.Jacksonized;
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
 @Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class UserDto {
     String id;
