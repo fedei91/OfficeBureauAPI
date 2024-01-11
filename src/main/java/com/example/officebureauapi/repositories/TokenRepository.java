d5a2782bdf6ec3ab43f5f2d254d30cb2249acf29
Federico <fedej.izaguirre@gmail.com>
2024-01-11 16:10:44 +0100

    perform refactoring regarding token query

--- src/main/java/com/example/officebureauapi/repositories/TokenRepository.java
+++ src/main/java/com/example/officebureauapi/repositories/TokenRepository.java
@@ -2,20 +2,11 @@ package com.example.officebureauapi.repositories;
 
 import com.example.officebureauapi.entities.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
-public interface TokenRepository extends JpaRepository<Token, Integer> {
-
-    @Query(value = """
-      select t from Token t inner join User u\s
-      on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
-      """)
-    List<Token> findAllValidTokenByUser(UUID id);
+public interface TokenRepository extends JpaRepository<Token, Integer>, JpaSpecificationExecutor<Token> {
 
     Optional<Token> findByToken(String token);
 }
