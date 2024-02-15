45905482bb57159aaac880f67b7d0842de81fffa
Federico <fedej.izaguirre@gmail.com>
2024-02-15 14:46:30 +0100

    add specification conflict check for timeslot

--- src/main/java/com/example/officebureauapi/repositories/ReservationRepository.java
+++ src/main/java/com/example/officebureauapi/repositories/ReservationRepository.java
@@ -4,10 +4,11 @@ import com.example.officebureauapi.entities.Reservation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+public interface ReservationRepository extends JpaRepository<Reservation, UUID>, JpaSpecificationExecutor<Reservation> {
     Page<Reservation> findByIsDeletedIsFalse(Pageable pageable);
     Page<Reservation> findByCreatedBy(UUID createdBy, Pageable pageable);
 }
